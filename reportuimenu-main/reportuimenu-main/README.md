9cb43dc1e3843e64722993f943a1e525ecd5669d
Hocejeking <53022454+Hocejeking@users.noreply.github.com>
2022-04-20 15:18:43 +0200

    Delete reportuimenu-main/reportuimenu-main directory

--- reportuimenu-main/reportuimenu-main/README.md
+++
@@ -1,4 +0,0 @@
-# ReportSystem
-Report system for KK rp <br>
-
-It is prohibited to use this resource without the owner permission.
--- reportuimenu-main/reportuimenu-main/config.lua
+++
@@ -1,6 +0,0 @@
-config = {}
-config.authorizedGroups = { 
-    ["admin"] = true
-}
-
-config.testurl = 'https://discord.com/api/webhooks/965216745218506813/H8lOG-zhOEwPg-dTZ8Z92xMRqpQLiawRtmbtTYp_aGK50wrV5slamdBzwL-L9ZvF5J7S'
